d25c4b4a21dd921dbc6ee43adea5a727b0fe3144
Bence Firtko <firtko.bence@student.uni-miskolc.hu>
2023-06-11 16:43:40 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CostumerRegistrationComponent } from './costumer-registration/costumer-registration.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
-import { CostumerListComponent } from './costumer-list/costumer-list.component';
 import { ToastrModule } from 'ngx-toastr';
-import { FoodListComponent } from './food-list/food-list.component';
 import { FoodFormComponent } from './food-form/food-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { OrderFormComponent } from './order-form/order-form.component';
-import { OvenListComponent } from './oven-list/oven-list.component';
+import { FoodListComponent } from './food-list/food-list.component';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CostumerRegistrationComponent,
-    CostumerListComponent,
-    FoodListComponent,
     FoodFormComponent,
-    OrderFormComponent,
-    OvenListComponent
+    FoodListComponent
   ],
   imports: [
     BrowserModule,
